e6069bf6a047020999ee35873d20c20d803bf42d
AweDNur <nurmwahyudewata@gmail.com>
2025-06-25 19:44:08 +0700

    first push

---
+++ Toko_MangUjang/src/main/java/com/mycompany/toko_mangujang/Toko_MangUjang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.toko_mangujang;
+
+/**
+ *
+ * @author DEWATA
+ */
+public class Toko_MangUjang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
